30fef2416e3c098d31af8c4bb2eb07490570c5e7
hcha0150 <hcha0150@student.monash.edu>
2024-12-11 13:09:35 +1100

    Enhance secure data sharing: add public key registration and retrieval functions

--- contracts/SecureStorage.sol
+++ contracts/SecureStorage.sol
@@ -5,20 +5,38 @@ contract DataSharing {
     struct Transaction {
         address sender;
         address receiver;
-        string encryptedData; // Encrypted message
+        string encryptedData;
         uint256 timestamp;
     }
 
     Transaction[] public transactions;
+    mapping(address => string) public publicKeys;
 
     event DataShared(address indexed sender, address indexed receiver, uint256 timestamp);
+    event PublicKeyRegistered(address indexed user);
 
     function shareData(address receiver, string memory encryptedData) public {
         transactions.push(Transaction(msg.sender, receiver, encryptedData, block.timestamp));
         emit DataShared(msg.sender, receiver, block.timestamp);
     }
 
-    function getTransactions() public view returns (Transaction[] memory) {
+    function registerPublicKey(string memory publicKey) public {
+    require(bytes(publicKey).length > 0, "Invalid public key");
+    publicKeys[msg.sender] = publicKey;
+    emit PublicKeyRegistered(msg.sender);
+    }
+
+    function storePublicKey(string memory publicKey) public {
+        publicKeys[msg.sender] = publicKey;
+        emit PublicKeyRegistered(msg.sender);
+    }
+
+
+    function getPublicKey(address user) public view returns (string memory) {
+        return publicKeys[user];
+    }
+
+    function getTransactions() public view returns (Transaction[] memory){
         return transactions;
     }
 }
